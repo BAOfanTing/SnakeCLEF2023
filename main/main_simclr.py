50f9d1096c486e78a687cb99586f83ef9177fef8
BAOfanTing <69379538+BAOfanTing@users.noreply.github.com>
2023-06-30 17:48:17 +0800

    Update main_simclr.py

--- main/main_simclr.py
+++ main/main_simclr.py
@@ -33,29 +33,29 @@ except ImportError:
 
 def parse_option():
     parser = argparse.ArgumentParser('MetaFG training and evaluation script', add_help=False)
-    parser.add_argument('--cfg', type=str, required=True, metavar="FILE", help='path to config file', )
+    parser.add_argument('--cfg', type=str, required=True, metavar="FILE", help='path to config file'， )
     parser.add_argument(
-        "--opts",
-        help="Modify config options by adding 'KEY VALUE' pairs. ",
-        default=None,
-        nargs='+',
+        "--opts"，
+        help="Modify config options by adding 'KEY VALUE' pairs. "，
+        default=None，
+        nargs='+'，
     )
 
     # easy config modification
     parser.add_argument('--batch-size', type=int, help="batch size for single GPU")
     parser.add_argument('--data-path',default='./imagenet', type=str, help='path to dataset')
     parser.add_argument('--zip', action='store_true', help='use zipped dataset instead of folder dataset')
-    parser.add_argument('--cache-mode', type=str, default='part', choices=['no', 'full', 'part'],
+    parser.add_argument('--cache-mode', type=str, default='part', choices=['no'， 'full'， 'part']，
                         help='no: no cache, '
                              'full: cache all data, '
                              'part: sharding the dataset into nonoverlapping pieces and only cache one piece')
     parser.add_argument('--resume', help='resume from checkpoint')
     parser.add_argument('--accumulation-steps', type=int, help="gradient accumulation steps")
-    parser.add_argument('--use-checkpoint', action='store_true',
+    parser.add_argument('--use-checkpoint', action='store_true'，
                         help="whether to use gradient checkpointing to save memory")
-    parser.add_argument('--amp-opt-level', type=str, default='O1', choices=['O0', 'O1', 'O2'],
+    parser.add_argument('--amp-opt-level', type=str, default='O1', choices=['O0'， 'O1'， 'O2']，
                         help='mixed precision opt level, if O0, no amp is used')
-    parser.add_argument('--output', default='output', type=str, metavar='PATH',
+    parser.add_argument('--output', default='output', type=str, metavar='PATH'，
                         help='root of output folder, the full path is <output>/<model_name>/<tag> (default: output)')
     parser.add_argument('--tag', help='tag of experiment')
     parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
@@ -111,7 +111,7 @@ def normalize(x, axis=-1):
 
 
 class InfoNCE(torch.nn.Module):
-    def __init__(self, batch_size=8, temperature=0.07, is_triplet=True):
+    def __init__(self, batch_size=8, temperature=0.25, is_triplet=True):
         super().__init__()
         self.temperature = temperature
         self.is_triplet = is_triplet
